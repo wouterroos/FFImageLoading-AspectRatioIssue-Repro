c091a5336d59505a0d7b70f78d3ef8aa35d52b2d
Wouter Roos <wouter.roos@qnh.nl>
2016-11-04 09:26:00 +0100

    Add project files.

---
+++ App3/App3.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using FFImageLoading.Forms.Droid;
+
+namespace App3.Droid
+{
+    [Activity(Label = "App3", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            CachedImageRenderer.Init();
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App3
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DetailPage());
+        }
+    }
+}
